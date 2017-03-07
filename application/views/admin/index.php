fb69fcd92a1619bd62d2fe93b8c31fcf5313d7b3
cicilestaris <cicilestarisupriatna@gmail.com>
2017-03-07 15:34:18 +0700

    up

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -47,13 +47,9 @@
 			<i class="icon-user"></i> <?php echo $this->session->userdata('username');?> <b class="caret"></b></a>
             <ul class="dropdown-menu">
 
-              <li><a data-toggle="modal" data-target="#myModal">Profile</a></li>
-			  
-			<!-- Trigger the modal with a button 
-			<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>-->
-               <!-- <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button> -->
-              <li><a href="javascript:; " data-toggle="modal" data-target="#myModal">Profile</a></li>
-             <li><a href="<?php echo site_url('home/logout'); ?>">Logout</a></li>
+            <li><a data-toggle="modal" data-target="#myModal">Profile</a></li>
+            <li><a href="<?php echo site_url('home/logout'); ?>">Logout</a></li>
+			 
             </ul>
           </li>
         </ul>
@@ -85,32 +81,6 @@
 <!-- /subnavbar -->
 <div class="main">
   <div class="main-inner">
-  <div class="container">
- 
-
-  <!-- Modal -->
-  <div class="modal fade" id="myModal" role="dialog">
-    <div class="modal-dialog">
-    
-      <!-- Modal content-->
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title">Modal Header</h4>
-        </div>
-        <div class="modal-body">
-          <p>Some text in the modal.</p>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        </div>
-      </div>
-      
-    </div>
-  </div>
-  
-</div>
-
     <div class="container">
 	
 		<!-- Modal -->
