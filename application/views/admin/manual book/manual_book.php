d72fc204e73ac5b169fc3a1b5821825ca46ac8d8
ELH\ellishidayati <ellis.hidayati@gmail.com>
2017-03-15 21:48:03 +0700

    up

--- application/views/admin/manual book/manual_book.php
+++ application/views/admin/manual book/manual_book.php
@@ -223,7 +223,7 @@ div.tab button.active {
   <div class="footer-inner">
     <div class="container">
       <div class="row">
-        <div class="span12"> &copy; 2013 <a href="http://www.egrappler.com/">Bootstrap Responsive Admin Template</a>. </div>
+        <div class="span12"> &copy; PLA UPI 2017</div>
         <!-- /span12 --> 
       </div>
       <!-- /row --> 
